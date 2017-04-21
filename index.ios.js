6fa66e7550d99b4be44bcd7abc5fa0eb0af84512
Andrii Uhryn <andriy.uhryn@thinkmobiles.com>
2017-04-21 06:46:41 +0300

    few updates

--- index.ios.js
+++ index.ios.js
@@ -1,17 +1,7 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+'use strict';
 
- import React, { Component } from 'react';
- import {
-     AppRegistry,
-     StyleSheet,
-     Text,
-     View
- } from 'react-native';
- import Weather from './src/containers/app';
+import React from 'react';
+import {AppRegistry} from 'react-native';
+import Weather from './src/containers/app';
 
-
- AppRegistry.registerComponent('Weather', () => Weather);
+AppRegistry.registerComponent('Weather', () => Weather);
