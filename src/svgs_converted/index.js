5c9e4eb1e4d9e3bca5fc25376372673ab1dfdedb
semakalexander <alexander_semak@mail.ua>
2017-05-03 17:11:48 +0300

    icons

--- src/svgs_converted/index.js
+++ src/svgs_converted/index.js
@@ -8,14 +8,37 @@ import Snowy from './snowy.js';
 import Storm from './storm.js';
 import AppIcon from './appIcon.js';
 
+import BigIceCloudy from './bigIceCloudy';
+import CloudyLittleSnow from './cloudyLittleSnow.js';
+import CloudySnow from './cloudySnow.js';
+import CloudySnowThunder from './cloudySnowThunder.js';
+import CloudyThunder from './cloudyThunder.js';
+import IceCloudy from './iceCloudy.js';
+import RainCloudy from './rainCloudy.js';
+import RainSnow from './rainSnow.js';
+import RainSnowSun from './rainSnowSun.js';
+import SnowThunder from './snowThunder.js';
+import Thunder from './thunder.js';
+
 export default {
-    back    : Back,
-    location: Location,
-    cloudy  : Cloudy,
-    fog     : Fog,
-    rainy   : Rainy,
-    snowy   : Snowy,
-    storm   : Storm,
-    sunny   : Sunny,
-    appIcon : AppIcon
+    back             : Back,
+    location         : Location,
+    cloudy           : Cloudy,
+    fog              : Fog,
+    rainy            : Rainy,
+    snowy            : Snowy,
+    storm            : Storm,
+    sunny            : Sunny,
+    appIcon          : AppIcon,
+    bigIceCloudy     : BigIceCloudy,
+    cloudyLittleSnow : CloudyLittleSnow,
+    cloudySnow       : CloudySnow,
+    cloudySnowThunder: CloudySnowThunder,
+    cloudyThunder    : CloudyThunder,
+    iceCloudy        : IceCloudy,
+    rainCloudy       : RainCloudy,
+    rainSnow         : RainSnow,
+    rainSnowSun      : RainSnowSun,
+    snowThunder      : SnowThunder,
+    thunder          : Thunder
 }
